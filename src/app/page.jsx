da2e21612e641072a510aa0789f3b4d35eeac88b
Viki2601 <mcvicky2601@gmail.com>
2025-06-30 07:35:12 +0530

    Analytics Added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,11 +5,28 @@ import Contact from "@/components/Contact";
 import Timeline from "@/components/Timeline";
 import Certificates from "@/components/Certificates";
 import Project from "@/components/Project";
-import { motion } from "framer-motion";
 import FooterName from "@/components/Footer";
 import Cursor from "@/common/Cursor";
+import Image from "next/image";
+import profilePic from "@/assets/img/Banner.svg";
+import { useRef, useState } from "react";
 
 export default function Home() {
+  const [isZoomed, setIsZoomed] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const imageRef = useRef(null);
+
+  const handleClick = () => {
+    setIsZoomed((prev) => !prev);
+  };
+
+  const handleMouseMove = (e) => {
+    if (!isZoomed) return;
+    const { left, top, width, height } = imageRef.current.getBoundingClientRect();
+    const x = ((e.pageX - left - window.scrollX) / width) * 100;
+    const y = ((e.pageY - top - window.scrollY) / height) * 100;
+    setPosition({ x, y });
+  };
   return (
     <>
       <Cursor />
@@ -32,6 +49,34 @@ export default function Home() {
         <Contact />
       </section>
       <FooterName />
+      <div
+        className="relative w-[400px] h-[400px] overflow-hidden cursor-zoom-in"
+        onClick={handleClick}
+        onMouseMove={handleMouseMove}
+        ref={imageRef}
+      >
+        <Image
+          src={profilePic}
+          alt="Astronaut"
+          fill
+          className={`object-contain select-none pointer-events-none transition-opacity duration-200 ${isZoomed ? 'opacity-0' : 'opacity-100'
+            }`}
+          priority
+        />
+
+        {isZoomed && (
+          <div
+            className="absolute inset-0 z-10 pointer-events-none"
+            style={{
+              backgroundImage: `url(${profilePic.src})`,
+              backgroundRepeat: 'no-repeat',
+              backgroundSize: '800px 800px',
+              backgroundPosition: `${position.x}% ${position.y}%`,
+              cursor: 'zoom-out',
+            }}
+          />
+        )}
+      </div>
     </>
   );
 }
